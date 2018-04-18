031f7c2b587f888a5ce4c08063e45987669d10d6
Dennis <dennisgeorge10@gmail.com>
2018-04-18 17:43:13 -0400

    correct endpoint, but CORS error

--- sendData.js
+++ sendData.js
@@ -4,37 +4,34 @@
 // example: endpoint=phrase&action=save&data=hello%20world
 
 
-
-
-
-
-function addParams() {
-    var url = "http://uptalk.tech/Uptalk/api.py?endpoint=phrase&action=save&data=";
-    var x = document.getElementById("inputTextArea").value;
-    var arr = x.split(" ");
-    var params = arr.join("%20");
-
-    return params;
-    // http.open("GET", url, true);
-
-
-    // http.send(params);
-}
-
-
-
 function sendNewUptalkPhrases(completionBlock) {
-	console.log("Hello World");
+	 console.log("Hello World");
     var phraseDataEndpoint = "http://uptalk.tech/Uptalk/api.py?endpoint=phrase&action=save&data=";
     var phraseDataEndpointWithParams = phraseDataEndpoint + addParams();
 
     console.log("sendNewUptalkPhrase phraseDataEndpoint = " + phraseDataEndpoint);
+
 
     $.get(phraseDataEndpoint, function(data) {
         completionBlock(data);
     }).fail(function(jqXHR, textStatus) {
         completionBlock(null);
     });
+
+    $(document).onload(function(){
+    		$("submitButton").click(function(){
+          var saveData = $('inputTextArea').val();
+          console.log(saveData);
+        		$.ajax({
+        			type: 'GET',
+        			url: 'http://uptalk.tech/Uptalk/api.py?endpoint=phrase&action=save',
+              data: { }
+        			success: function(result){
+
+            			$("inputTextArea").html(result);
+        		}});
+    		});
+		});
 }
 
 
